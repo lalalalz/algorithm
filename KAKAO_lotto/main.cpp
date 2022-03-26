1509ed8bcd05c0b0a6209ff9b0d725678fb2b583
lalalalz <lalalalz@naver.com>
2022-03-26 17:04:08 +0900

    # ADD # KAKAO 로또 순위의 최고순위와 최저순위

---
+++ KAKAO_lotto/main.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> getCounts(vector<int> lottos, vector<int> win_nums)
+{
+    int zero = 0;
+    int hits = 0;
+    vector<int> result;
+
+    for (int i = 0; i < win_nums.size(); ++i)
+    {
+        if(lottos[i] == 0)  zero++;
+        for (int j = 0; j < lottos.size(); ++j)
+        {
+            if(lottos[j] == win_nums[i])
+            {
+                hits++;
+                break;
+            }
+        }
+    }
+
+    result.push_back(hits);
+    result.push_back(zero);
+    return result;
+}
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums)
+{
+    vector<int> answer;
+    vector<int> counts = getCounts(lottos, win_nums);
+
+    int max = (7 - counts[0] + counts[1]) < 6 ? (7 - counts[0] + counts[1]) : 6;
+    int min = (7 - counts[0] < 6) ? (7 - counts[0]) : 6;
+
+    answer.push_back(max);
+    answer.push_back(min);
+
+    return answer;
+}
+
